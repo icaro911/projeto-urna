b2b741fc50a29d5fb2fae7563313875715fb2a49
Ícaro Cruz <68250279+icaro911@users.noreply.github.com>
2022-08-31 14:41:52 -0300

    Readequando o projeto URNA
    Retirando de outra pasta como portifólio.

---
+++ scripts/etapas.js
@@ -0,0 +1,57 @@
+let etapas = [
+	{
+		titulo: "VEREADOR",
+		numeros: 5,
+		candidatos: [
+			{
+				numero: '38111',
+				nome: 'Gaguinho',
+				partido: 'Roça',
+				fotos: [
+					{url:'/photos/edgard.jpg', legenda: 'Vereador'}
+				]
+			},
+			{
+				numero: '72222',
+				nome: 'Pepe Le Pew',
+				partido: "Je t'aime",
+				fotos: [
+					{url:'/photos/pepelepew.jpg', legenda: 'Vereador'}
+				]
+			},
+		]
+		
+	},
+	{
+		titulo: "PREFEITO",
+		numeros: 2,
+		candidatos: [
+			{
+				numero: '99',
+				nome: 'Eufrasino',
+				partido: 'Bang-Bang',
+				vice: 'Hortelino',
+				fotos: [
+					{url:'/photos/eu.jpg', legenda: 'Prefeito'},
+					{url:'/photos/eu2.jpg', legenda: 'Vice-Prefeito', small: true}
+				]
+				
+			},
+			{
+				numero: '84',
+				nome: 'Pernalonga',
+				partido: 'QQA-velinho',
+				vice: 'Patolino',
+				fotos: [
+					{url:'/photos/perna.jpg', legenda: 'Prefeito'},
+					{url:'/photos/pato.jpg', legenda: 'Vice-Prefeito', small: true}
+				]
+				
+			},
+		
+		]
+		
+		
+	}
+
+];
